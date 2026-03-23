954d9356bd6247281081ef669337ddda6ec22cff
monil003 <mdumasia@oncloudconsulting.ca>
2026-03-23 16:35:03 -0400

    admin project edit and delete APIs

--- routes/projects.js
+++ routes/projects.js
@@ -34,4 +34,42 @@ router.post('/', [auth, admin], async (req, res) => {
   }
 });
 
+// Update a project (Admin Only)
+router.put('/:id', [auth, admin], async (req, res) => {
+  try {
+    const { name, clientOrTask } = req.body;
+    let project = await Project.findById(req.params.id);
+
+    if (!project) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+
+    project.name = name || project.name;
+    project.clientOrTask = clientOrTask || project.clientOrTask;
+
+    await project.save();
+    res.json(project);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// Delete a project (Admin Only)
+router.delete('/:id', [auth, admin], async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+
+    if (!project) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+
+    await Project.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Project removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
