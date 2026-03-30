c30cfd79815ec275dd2ac4e6e31b9342d5bb3b41
monil003 <mdumasia@oncloudconsulting.ca>
2026-03-30 17:02:35 -0400

    remove logs

--- routes/auth.js
+++ routes/auth.js
@@ -7,14 +7,6 @@ const User = require('../models/User');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
-// ---- Email transporter ----
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
